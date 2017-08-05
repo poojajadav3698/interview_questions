f933b5f070aebffa4865bd554f1d976014b8ea98
vidurac <viduraucsc@gmail.com>
2017-08-05 20:23:28 +1200

    tuned the code

--- question2.php
+++ question2.php
@@ -10,29 +10,38 @@ require_once 'command.php';
 
 if(PHP_OS === "Linux")
 {
-	$image_magic_obj = new imageMagic("/usr/local/imagemagick");
-	echo $image_magic_obj->getImageMagicPath();
+	$image_magic_obj = new ImageMagic("/usr/local/imagemagick");
+	$isValidPath = $image_magic_obj->isValidPath();
 
-	//initialize the command class
-	$command_obj = new command("convert -limit memory 0 -limit map 0 -quiet ",
-		"profile.jpg",
-		" -thumbnail @11000 -quality 50 ",
-		"output.png"
-	);
+	if($isValidPath)
+	{
+		//initialize the command class
+		$command_obj = new Command(
+			"convert -limit memory 0 -limit map 0 -quiet ",
+			"profile.jpg",
+			" -thumbnail @11000 -quality 50 ",
+			"output.png"
+		);
 
-	//get base path
-	$command = $command_obj->getBaseCommand();
-	//get original file path
-	$command .= $command_obj->getOriginPath();
-	//get parameters
-	$command .= $command_obj->getParams();
-	//get target path
-	$command .= $command_obj->getTargetPath();
+		//get base path
+		$command = $command_obj->getBaseCommand();
+		//get original file path
+		$command .= $command_obj->getOriginPath();
+		//get parameters
+		$command .= $command_obj->getParams();
+		//get target path
+		$command .= $command_obj->getTargetPath();
 
-	//run the command
-	$output = shell_exec($command);
+		//run the command
+		$output = shell_exec($command);
 
-	return $output;
+		return $output;
+	}
+	else
+	{
+		return "Could not found image magic executable path.";
+	}
+
 }
 else if(PHP_OS === "Windows")
 {
