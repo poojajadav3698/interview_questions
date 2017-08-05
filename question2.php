ce9e01203a8c4c0c9e8078550f89177eba038470
vidurac <viduraucsc@gmail.com>
2017-08-05 23:48:04 +1200

    tuned the code

--- question2.php
+++ question2.php
@@ -8,45 +8,39 @@
 require_once 'imageMagic.php';
 require_once 'command.php';
 
-if(PHP_OS === "Linux")
-{
-	$image_magic_obj = new ImageMagic("/usr/local/imagemagick");
-	$isValidPath = $image_magic_obj->isValidPath();
+if (PHP_OS === "Linux") {
+    $image_magic_obj = new ImageMagic("/usr/local/imagemagick");
+    $isValidPath = $image_magic_obj->isValidPath();
 
-	if($isValidPath)
-	{
-		//initialize the command class
-		$command_obj = new Command(
-			"convert -limit memory 0 -limit map 0 -quiet ",
-			"profile.jpg",
-			" -thumbnail @11000 -quality 50 ",
-			"output.png"
-		);
+    if ($isValidPath) {
+        //initialize the command class
+        $command_obj = new Command(
+            "convert -limit memory 0 -limit map 0 -quiet ",
+            "profile.jpg",
+            " -thumbnail @11000 -quality 50 ",
+            "output.png"
+        );
 
-		//get ImageMagic Path
-		//$command = $image_magic_obj->getImageMagicPath();
-		//get base path
-		$command = $command_obj->getBaseCommand();
-		//get original file path
-		$command .= $command_obj->getOriginPath();
-		//get parameters
-		$command .= $command_obj->getParams();
-		//get target path
-		$command .= $command_obj->getTargetPath();
+        //get ImageMagic Path
+        //$command = $image_magic_obj->getImageMagicPath();
+        //get base path
+        $command = $command_obj->getBaseCommand();
+        //get original file path
+        $command .= $command_obj->getOriginPath();
+        //get parameters
+        $command .= $command_obj->getParams();
+        //get target path
+        $command .= $command_obj->getTargetPath();
 
-		//run the command
-		$output = $command_obj->execute($command);
+        //run the command
+        $output = $command_obj->execute($command);
 
-		return $output;
-	}
-	else
-	{
-		return "Could not found image magic executable path.";
-	}
+        return $output;
+    } else {
+        return "Could not found image magic executable path.";
+    }
 
-}
-else if(PHP_OS === "Windows")
-{
+} else if (PHP_OS === "Windows") {
 
 }
 ?>
