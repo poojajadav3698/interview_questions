22ac72bd112c0cc57f388ea2873a8172b41625f4
vidurac <viduraucsc@gmail.com>
2017-08-05 23:39:32 +1200

    question 1 - modified the code

--- question1.php
+++ question1.php
@@ -7,48 +7,12 @@
  */
 require_once 'scanDirectory.php';
 
-$file_names = array();
+/**
+ * Initialize ScanDirectory class
+ */
+$scan_obj = new ScanDirectory(
+    'my_directory'//path of the directory
+);
 
-function getFileNames($dir)
-{
-    //echo "call get file names"."<br>";
-    $scan_obj = new ScanDirectory(
-        $dir
-    );
-
-    if($scan_obj->isValidPath())
-    {
-        $contents = $scan_obj->loadFiles();
-
-        if(count($contents) > 0)
-        {
-            foreach($contents as $content)
-            {
-                if($scan_obj->isDirectory($content))
-                {
-                    $file_names[] = array("is_directory" => true, "name" => $content);;
-                    //echo ($content)." ".$scan_obj->isDirectory($content)."<br> ";
-                    $sub_dir_path = $dir.DIRECTORY_SEPARATOR.$content;
-                    //echo $sub_dir_path."<br>";
-                    getFileNames($sub_dir_path);
-                }
-                else
-                {
-                    echo ($content);
-                    echo "<br>";
-                    $file_names[] = array("is_directory" => false, "name" => $content);
-                }
-            }
-        }
-
-        return $file_names;
-    }
-    else
-    {
-        return "Invalid file path.";
-    }
-
-}
-
-echo json_encode(getFileNames('my_directory'));
+print_r($scan_obj->printAllFilesAndDirectories());
 
